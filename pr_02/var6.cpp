1f3c4f38b74e6532ce9ce2f01ac9cc7833a2c09d
Rusal84 <rusal@bk.ru>
2021-02-19 10:30:56 +0300

    pr 02

---
+++ pr_02/var6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+string Lessons[5];
+
+struct Student {
+    char FIO[40];
+    int Marks[5];
+};
+
+
+int main() {
+    Lessons[0] = "математика";
+    Lessons[1] = "физика";
+    Lessons[2] = "черчение";
+    Lessons[3] = "химия";
+    Lessons[4] = "сопромат";
+    cout << "ВВедите количество студентов";
+
+    Student MasOfStudents[n];
+
+    cout << "ВВедите ФИО студента";
+
+}

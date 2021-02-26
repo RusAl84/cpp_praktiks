bda782d69cf6811e7add31b14d92287d12a45485
Rusal84 <rusal@bk.ru>
2021-02-26 15:02:55 +0300

    add testsound beep 2

---
+++ Off_Top/TestBeepSound2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 30; i > 10; i--) {
+        Beep(5000 - 100*i, 100+i*10);
+        std::cout << i;
+    }
+    Beep(3000 , 20  * 1000);
+    
+    std::cout << "Hello World!\n";
+}
+

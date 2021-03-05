c08e84c5c733fdac9fe5e2eb1b23b584a23ef0fb
Rusal84 <rusal@bk.ru>
2021-03-05 14:32:20 +0300

    add var 2

---
+++ pr_04/pr_04_var/pr_04_var.cpp
@@ -0,0 +1,51 @@
+﻿// 2  variant
+
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+	int value;
+	node* next;
+	node* prev;
+};
+
+static node* head;
+static int count;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	// Dannie kot vvodim
+	float mas[4];
+	mas[0] = 0.1;
+	mas[1] = 1.1;
+	mas[2] = 2.1;
+	mas[3] = 3.1;
+	head = NULL;
+
+
+	for (int i = 0; i < 4; i++) {
+		int value = mas[i];
+		if (!head)
+		{
+			head = new node;
+			head->value = value;
+			head->next = NULL;
+			head->prev = NULL;
+
+		}
+		else
+		{
+			node* tmp = new node;
+			tmp->value = value;
+			tmp->next = head;
+			tmp->prev = NULL;
+			head = tmp;
+			head->next->next->prev = head->next;
+		}
+	}
+    std::cout << "Hello World!\n";
+}

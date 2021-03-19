473c160dcf4bb04d7fa82c5064a31ab778ea59fb
Rusal84 <rusal@bk.ru>
2021-03-19 15:58:14 +0300

    var3 + ptr ex

--- pr_06/pr_06_var3/ConsoleApplication1/ConsoleApplication1.cpp
+++ pr_06/pr_06_var3/ConsoleApplication1/ConsoleApplication1.cpp
@@ -15,12 +15,18 @@ int main()
     //stud->Display();
     return 0;
 
-
     //ClassVar3* stud1 = new ClassVar3();
     //stud1->FIO = "Filatov";
     //stud1->Age = 46;
     //stud1->Display();
-}
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-//
+
+    int ind = 0;
+    int* pint;
+    pint = &ind;
+    *pint = 8;
+    //pint = &8;
+    int* pint1 = new int(1111);
+    *pint1 = 2222;
+    cout << &pint1;
+}

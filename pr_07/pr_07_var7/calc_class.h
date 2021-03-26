97702da2f0d86f8b47a75c9a73515b9ec1cd4a4d
Rusal84 <rusal@bk.ru>
2021-03-26 13:36:21 +0300

    add pr7 var 7

---
+++ pr_07/pr_07_var7/calc_class.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <cmath>
+#include <iostream>
+
+class calc_class
+{
+private:
+	double x;
+	double y;
+	double z;
+	double gamma;
+	double result;
+	int variant;
+public:
+	calc_class(int _var) {
+		variant = _var;
+	}
+	calc_class() {
+		variant = 7;
+	}
+	void set(double _x, double _y) {
+		x = _x;
+		y = _y;
+	}
+	void set() {
+		switch (variant)
+		{
+		case 7: {
+			x = 0.1722;
+			y = 6.33;
+			z = 3.25 * pow(10, -4);
+			gamma = -205.305;
+			break; }
+		default:
+			break;
+		}
+	}
+	void run() {
+		switch (variant)
+		{
+		case 7:{
+			double chisl = exp(abs(x - y)) * pow(abs(x - y), (x + y));
+			double znam = atan(x) + atan(z);
+			double slagaemoe = pow((pow(x,6)+ log(y)* log(y)), 1 / 3);
+			result = chisl / znam + slagaemoe;
+			break; }
+		default:
+			break;
+		}
+	}
+	void dislay() {
+		std::cout << "Result = " << result << std::endl;
+	}
+
+
+};
+
---
+++ pr_07/pr_07_var7/pr_07_var7.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include "calc_class.h"
+
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Hello World!\n";
+    calc_class *calc = new calc_class(7);
+    calc->set();
+    calc->run();
+    calc->dislay();
+
+}
+

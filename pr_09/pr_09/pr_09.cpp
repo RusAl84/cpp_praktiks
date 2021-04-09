4bfb67ad9b3ea3b06640c83d47b8ece9a813f1b9
Rusal84 <rusal@bk.ru>
2021-04-09 14:06:07 +0300

    var 2

--- pr_09/pr_09/pr_09.cpp
+++ pr_09/pr_09/pr_09.cpp
@@ -8,36 +8,33 @@ static void display(string filename) {
     string line;
     ifstream in(filename); // окрываем файл для чтения
     if (in.is_open())
-    {
         while (getline(in, line))
-        {
             std::cout << line << std::endl;
-        }
-    }
     in.close();     // закрываем файл
 }
 // Генерируем рандомное число между значениями min и max.
 // Предполагается, что функцию srand() уже вызывали
-int getRandomNumber(int min, int max)
+static int getRandomNumber(int min, int max)
 {
     static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
     // Равномерно распределяем рандомное число в нашем диапазоне
     return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
-
-int main()
+static double getRandomdoubleNumber (double LO, double HI)
 {
-    srand(static_cast<unsigned int>(time(0))); // устанавливаем значение системных часов в качестве стартового числа
-    setlocale(LC_ALL, "");
+    return LO + static_cast <double> (rand()) / (static_cast <double> (RAND_MAX / (HI - LO)));
+}
+static void var7() {
+    int count = 10; // Количество записей
     std::ofstream out;          // поток для записи
     out.open("D:\\hello.txt"); // окрываем файл для записи
     if (out.is_open())
     {
-        int count = 10;
-        for(int i=0;i<count;i++){
-            int random_number = getRandomNumber(1,100000);
+
+        for (int i = 0; i < count; i++) {
+            int random_number = getRandomNumber(100, 700);
             out << random_number << std::endl;
-        }  
+        }
     }
     out.close();
     cout << "Файл сгенерирован: " << endl;
@@ -45,16 +42,13 @@ int main()
     cout << "Выполненное задание: " << endl;
     string line;
     ifstream in("D:\\hello.txt"); // окрываем файл для чтения
-
-
-
     //    Записать в файл g комментарий.
-    out.open("D:\\out.txt"); // окрываем файл для запис
+    out.open("D:\\out.txt"); // окрываем файл для записи
     if (in.is_open())
     {
         while (getline(in, line))
         {
-           /* std::cout << line << std::endl;*/
+            /* std::cout << line << std::endl;*/
             int number = atoi(line.c_str());
             string comment = " //";
             //cout << number << endl;
@@ -64,7 +58,7 @@ int main()
             }
             //2. делящиеся на 3 и не делящиеся на 7;
             if ((number % 3 == 0) && !(number % 7 == 0)) {
-                if (comment == " //") 
+                if (comment == " //")
                     comment = " // делится на 3 и не делится на 7";
                 else
                     comment += ", делится на 3 и не делится на 7";
@@ -92,5 +86,82 @@ int main()
     out.close();
     in.close();     // закрываем файл
     display("D:\\out.txt");
+}
+
+
+static void var2() {
+    int count = 10; // Количество записей
+    std::ofstream out;          // поток для записи
+    out.open("D:\\hello.txt"); // окрываем файл для записи
+    if (out.is_open())
+    {
+        for (int i = 0; i < count; i++) {
+            double random_number = getRandomdoubleNumber(0.05, 0.7);
+            out << random_number << endl;
+        }
+    }
+    out.close();
+    cout << "Файл сгенерирован: " << endl;
+    display("D:\\hello.txt");
+    cout << "Выполненное задание: " << endl;
+    //Найти:
+    //5. разность первой и последней компоненты файла f.
+    string line;
+    ifstream in("D:\\hello.txt"); // окрываем файл для чтения
+    //    Записать в файл g комментарий.
+    out.open("D:\\out.txt"); // окрываем файл для записи
+    if (in.is_open())
+    {
+        double max_num = -10000;
+        double max_abs_num = -10000;
+        double min_num_u = 10000;
+        double min_num = 10000;
+        double first_num;
+        double last_num;
+        bool flag = true;
+        while (getline(in, line))
+        {
+            if (flag) {
+                flag = false;
+                first_num = atof(line.c_str());
+            }
+            /* std::cout << line << std::endl;*/
+            double number = std::stof(line.c_str());
+            std::cout << number << std::endl;
+            string comment = " //";
+            //1. наибольшее из значений компонентов f;
+            if (number > max_num)
+                max_num = number;
+            //2. наименьшее из значений компонентов с четными номерами;
+            if ((int)number % 2 == 0)
+                if (number < min_num_u)
+                    min_num_u = number;
+            //3. наибольшее из значений модулей компонентов с нечетными номерами;
+            if ((int)number % 2 == 1)
+                if (abs(number) > max_abs_num)
+                    max_abs_num = abs(number);
+            //4. сумму наибольшего и наименьшего из значений компонентов файла f;
+            if (number < min_num)
+                min_num = number;
+            //out << number << comment << endl;
+            last_num = number;
+        }
+        cout << "1. наибольшее из значений компонентов f: " << max_num << endl;
+        cout << "2. наименьшее из значений компонентов с четными номерами: " << min_num_u << endl;
+        cout << "3. наибольшее из значений модулей компонентов с нечетными номерами: " << max_abs_num << endl;
+        cout << "4. сумму наибольшего и наименьшего из значений компонентов файла f: " << max_num - min_num << endl;
+        cout << "5. разность первой и последней компоненты файла f:" << first_num - last_num << endl;
+
+    }
+    out.close();
+    in.close();     // закрываем файл
+    display("D:\\out.txt");
+}
+
+int main()
+{
+    srand(static_cast<unsigned int>(time(0))); // устанавливаем значение системных часов в качестве стартового числа
+    setlocale(LC_ALL, "");
+    var2();
 }
 

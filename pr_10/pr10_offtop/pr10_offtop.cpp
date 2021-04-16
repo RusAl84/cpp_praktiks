fe4764f796455a9b3c4d956d0e1c1aa2324acfc2
Rusal84 <rusal@bk.ru>
2021-04-16 13:40:36 +0300

    add example of comments using

---
+++ pr_10/pr10_offtop/pr10_offtop.cpp
@@ -0,0 +1,22 @@
+﻿// pr10_offtop.cpp : Пример комментариев
+// rusal84
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/// <summary>
+/// Функция отправки в армию
+/// </summary>
+/// <param name="groupName">Название группы студентов (БАСО-01-20)</param>
+/// <returns>Число годных новобранцев</returns>
+int goArmy(string groupName) {
+    return 10;
+}
+
+int main()
+{
+    int countRecruit = goArmy("БАСО-05-20");
+    std::cout << "Hello World!\n";
+}

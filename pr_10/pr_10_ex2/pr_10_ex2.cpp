c3e66664d6cc009a81fce2541cd851630452a680
Rusal84 <rusal@bk.ru>
2021-04-16 13:31:26 +0300

    pr_10_ex2

---
+++ pr_10/pr_10_ex2/pr_10_ex2.cpp
@@ -0,0 +1,24 @@
+﻿// pr_10_ex2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Human.h"
+
+int main()
+{
+    // создание объекта
+    Human human_obj = Human("Lavrov  ministr$)");
+   // for(int i=0;i<10;i++)
+   // {
+        human_obj.saySomething();
+   // }
+   
+
+    Human* human_dyn_obj = new Human();
+    human_dyn_obj->known_phrase = "Ya devochka";
+    std::cout << std::endl;
+    human_dyn_obj->saySomething();
+
+    return 0;
+}
+

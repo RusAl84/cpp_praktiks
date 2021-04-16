d89b08205d90767d469a2021bef8ca32f7083d18
Rusal84 <rusal@bk.ru>
2021-04-16 13:51:22 +0300

    pr_10_ex4

---
+++ pr_10/pr_10_ex4/pr_10_ex4.cpp
@@ -0,0 +1,55 @@
+﻿#include "iostream"
+#include <vector>
+
+// прототип класса
+class PhraseRobber;
+
+class Human {
+private:
+    friend PhraseRobber;
+    // поле класса
+    std::string known_phrase;
+
+public:
+    // конструктор класса
+    Human(std::string phrase) {
+        known_phrase = phrase;
+
+    }
+
+    // метод класса
+    void saySomething() {
+        std::cout << known_phrase;
+    }
+
+    // деструктор класса
+    ~Human() {
+        std::cout << "Bye world";
+    }
+
+};
+
+
+// дружественный класс
+class PhraseRobber {
+public:
+    std::string steal_phrase(Human human) {
+        std::string temp = human.known_phrase;
+        human.known_phrase = "phrase was stolen";
+        return temp;
+    }
+};
+
+
+
+
+int main() {
+
+    // создание объекта
+    Human human_obj = Human("Hello world\n");
+    human_obj.saySomething();
+
+
+    return 0;
+}
+// был вызан деструктор

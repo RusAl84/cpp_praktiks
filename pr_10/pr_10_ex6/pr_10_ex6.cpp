dd549ec536e35937c588aff26b627a7d12680296
Rusal84 <rusal@bk.ru>
2021-04-16 14:03:05 +0300

    pr10_ex6

---
+++ pr_10/pr_10_ex6/pr_10_ex6.cpp
@@ -0,0 +1,32 @@
+﻿#include "iostream"
+
+
+class Human {
+public:
+
+    virtual void saySomething() {
+        std::cout << "I'm Human";
+    }
+
+
+};
+
+class Children : Human {
+public:
+    virtual void saySomething() override {
+        std::cout << "I'm Children";
+
+    }
+
+};
+
+
+int main() {
+
+    Human().saySomething();
+    std::cout << std::endl;
+    Children().saySomething();
+
+    return 0;
+}
+

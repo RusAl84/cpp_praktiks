a29f403a786e17d3da00b3211215c8958a376772
Aleksey <rusal@bk.ru>
2022-02-24 13:30:55 +0300

    fix

---
+++ pr_03/pr_03/offtop/sema_test/sema_test/sema_test.cpp
@@ -0,0 +1,32 @@
+﻿// sema_test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    struct student {
+        string name;
+        int loadCapacity;
+        string drinkType;
+    };
+    student *sema = new student();
+    sema->name = "Семён шалун";
+    sema->drinkType = "Водка absolut";
+    sema->loadCapacity = 800;
+    student* dima = new student();
+    dima->name = "Дима засланец";
+    dima->drinkType = "Henessy XO";
+    dima->loadCapacity = 600;
+
+    vector <student> tripNaDachuMihalicha;
+    tripNaDachuMihalicha.push_back(*sema);
+    for (auto item : tripNaDachuMihalicha) {
+        cout << item.name << item.drinkType << item.loadCapacity << endl;
+    }
+
+
+
+    std::cout << "Hello World!\n";
+}
---
+++ pr_03/pr_03/offtop/test_vector2/test_vector2/test_vector2.cpp
@@ -0,0 +1,50 @@
+﻿// test_vector2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    struct student {
+        string name;
+        float weight;
+        string drink;
+    };
+
+    student *stepa = new student();
+    student *nikita = new student();
+    student *lenochka = new student();
+    
+    stepa->name = "Спепка заводной";
+    stepa->drink = "Абсент";
+    stepa->weight = 0.33;
+    nikita->name = "Никита глаза на затылке";
+    nikita->drink = "Henessy XO";
+    nikita->weight = 0.7;
+    lenochka->name = "Леночка - наше солнышко;)";
+    lenochka->drink = "Martini Biyanco";
+    lenochka->weight = 0.3 ;
+
+    vector <student> tripDachaMihalicha;
+
+    tripDachaMihalicha.push_back(*stepa);
+    tripDachaMihalicha.push_back(*nikita);
+    tripDachaMihalicha.push_back(*lenochka);
+
+    cout << tripDachaMihalicha.size();
+
+    for (auto i : tripDachaMihalicha){
+        cout << endl << i.weight;
+        //cout << endl << i;
+    }
+
+
+
+    float all_weight = 0;
+    for (int i = 0; i < tripDachaMihalicha.size(); i++) {
+        all_weight += tripDachaMihalicha[i].weight;
+    }
+    cout << endl <<all_weight;
+}

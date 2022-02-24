35daee71c20a063fd9523c0ea3186a64f1a28e7b
Aleksey <rusal@bk.ru>
2022-02-24 09:47:12 +0300

    ex1

--- Off_Top/KillComp/KillComp/KillComp.cpp
+++ Off_Top/KillComp/KillComp/KillComp.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <windows.h>
 
-
 using namespace std;
 
 int main()
---
+++ pr_03/pr_03/offtop/test_vector/test_vector/test_vector.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main()
+{
+    struct student {
+        string name;
+        float drink;
+    };
+    vector <student> bandaMihalicha;
+    student *kostya = new student();
+    kostya->name = "Костя с шарфиком";
+    kostya->drink = 0.6;
+
+    student* dima = new student();
+    dima->name = "Дима SAI";
+    dima->drink = 0.8;
+
+    bandaMihalicha.push_back(*kostya);
+    bandaMihalicha.push_back(*dima);
+
+
+    cout << bandaMihalicha.size();
+}
+

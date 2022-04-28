3e5e8ae6042c9cb11c4143aa3b353d80f01082ed
Aleksey <rusal@bk.ru>
2022-04-28 14:06:45 +0300

    fix

--- pr_12/8/pr_12_8/pr_12_8.cpp
+++ pr_12/8/pr_12_8/pr_12_8.cpp
@@ -1,10 +1,23 @@
 ﻿
 #include <iostream>
+#include "Maxtrix3Class.h"
 
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Maxtrix3Class *m1 = new Maxtrix3Class();
+    Maxtrix3Class *m2 = new Maxtrix3Class();
+    int inc = 0;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+        {
+            m1->matrix[i][j]=inc;
+            m2->matrix[i][j]=3;
+            inc++;
+        }
+
+    m1->Print();
+    m2->Print();
 }
 

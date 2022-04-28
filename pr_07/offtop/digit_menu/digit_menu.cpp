2433bc3d4960bfd19413dae0b6a4d8ca890d3c09
Aleksey <rusal@bk.ru>
2022-04-28 14:01:12 +0300

    add 12 pract

---
+++ pr_07/offtop/digit_menu/digit_menu.cpp
@@ -0,0 +1,54 @@
+﻿// digit_menu.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct exam {
+    string subject[100];
+    int mark; // 0 - не зачтено
+              // 1 - зачтено
+              // 2 - 2; 3 - 3; 4 - 4; 5 - 5;
+    bool isEmpty;
+};
+struct student {
+    char name[30];
+    char group[10];
+    exam sessData[10][9];
+};
+
+void AddStudData() {
+    student st;
+
+    cout << "Введите Имя:";
+    string bufer;
+    cin >> bufer;
+    strcpy(st.name, bufer.c_str());
+
+    cout << "Введите группу:";
+    cin >> bufer;
+}
+
+
+int main()
+{   
+    setlocale(LC_ALL, "Russian");
+    int chouse = -1;
+    while (chouse != 0) {
+        cout << "Введите 0 для выхода" << endl;
+        cout << "Введите 1 для добавления данных о студенте" << endl;
+        cin >> chouse;
+        switch (chouse) {
+        case 0:
+            break;
+        case 1: 
+            AddStudData();
+            break;
+        default:
+            break;
+        }
+    }
+
+    std::cout << "Конец програмыы\nВы все умрете...\n";
+}
---
+++ pr_12/8/pr_12_8/pr_12_8.cpp
@@ -0,0 +1,10 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+

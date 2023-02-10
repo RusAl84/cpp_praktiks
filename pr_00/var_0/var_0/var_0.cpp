e83252036b745f04e2c0bc69fa548f99442a39a0
Aleksey <rusal@bk.ru>
2023-02-10 13:50:56 +0300

    add getSpaceBar

--- pr_00/var_0/var_0/var_0.cpp
+++ pr_00/var_0/var_0/var_0.cpp
@@ -1,29 +1,44 @@
 ﻿// var_0.cpp : 
-
 #include <iostream>
 #include <windows.h>
 
 using namespace std;
 
-
 void DrawLine() {
 	for (int i = 0; i < 80; i++) {
 		cout << "-";
 	}
-		
+	cout << "\n";
 }
+
+char* GetSpacebar(int count) {
+	char* msg = new char[count];
+	for (int i = 0; i < count; i++)
+	{
+		msg[i] = ' ';
+	}
+	msg[count] = '\0';
+	return msg;
+}
+
+
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+	struct Line
+	{
+		char surName[17]; 
+		char ident[12];
+		unsigned short year;
+		double salary;
+	};
+	struct Line line1 = { "Иванов", "И. И.", 1975, 517.50 };
+	struct Line line2 = { "Петренко", "П.П.", 1956, 219.10 };
+	struct Line line3 = { "Панковский", "М.С.", 1967, 300.00 };
 
-	char surName1[17], surname2[17], surname3[17];
-	char initials1[12], initials2[12], initials3[12];
-	unsigned short year1, year2, year3;
-	float salary1, salary2, salary3;
-
-
-    cout << "Hello World!\n";
+	DrawLine();
+	cout << "| Отдел кадров" <<  GetSpacebar(80-15) << "|\n";
 	DrawLine();
 }

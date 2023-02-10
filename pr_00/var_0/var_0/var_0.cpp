f424f809a8dd4c8c2dc7181a975b4a409c5324f7
Aleksey <rusal@bk.ru>
2023-02-10 13:12:36 +0300

    add tasks

---
+++ pr_00/var_0/var_0/var_0.cpp
@@ -0,0 +1,29 @@
+﻿// var_0.cpp : 
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+void DrawLine() {
+	for (int i = 0; i < 80; i++) {
+		cout << "-";
+	}
+		
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char surName1[17], surname2[17], surname3[17];
+	char initials1[12], initials2[12], initials3[12];
+	unsigned short year1, year2, year3;
+	float salary1, salary2, salary3;
+
+
+    cout << "Hello World!\n";
+	DrawLine();
+}

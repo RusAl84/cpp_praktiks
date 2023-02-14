637859a82a3883525e77c4ae9ead14fe0b49d7a2
Aleksey <rusal@bk.ru>
2023-02-14 15:50:18 +0300

    fix

--- Off_Top/gameExClass/gameExClass.cpp
+++ Off_Top/gameExClass/gameExClass.cpp
@@ -6,5 +6,13 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    struct book {
+        char title[81];
+        char author[30];
+        int pageCount;
+    };
+    struct book cpp1 = { "Программирование на C++", "Русаков Алексей", 3 };
+
+    cout << cpp1.author << endl << cpp1.title << endl << cpp1.title;
+    //std::cout << "Hello World!\n";
 }

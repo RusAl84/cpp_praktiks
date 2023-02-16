63050652a3614d6bbb21980c63454388ba802c97
Aleksey <rusal@bk.ru>
2023-02-16 14:10:13 +0300

    add mas Records

--- pr_00/var_0/var_0/var_0.cpp
+++ pr_00/var_0/var_0/var_0.cpp
@@ -15,6 +15,29 @@ struct Record
 };
 
 
+struct Record CreateRecord() {
+	struct Record newRecord;
+	// ToDo cin 
+
+	return newRecord;
+}
+
+struct Record ReadRecord(struct Record *records, int index) {
+	struct Record newRecord;
+	// ToDo cin 
+	return newRecord;
+}
+
+
+struct Record UpdateRecord(struct Record inRecord, int index) { 
+	return inRecord;
+}
+
+void deleteRecord(struct Record inRecord, int index) {
+	
+}
+
+
 void DrawLine() {
 	for (int i = 0; i < 80; i++) {
 		cout << "-";
@@ -39,27 +62,26 @@ int GetSize(char* msg) {
 	return size;
 }
 
+void Draw(struct Record * records) {
 
-struct Record CreateRecord() {
-	struct Record newRecord;
-	// ToDo cin 
-
-	return newRecord;
-}
-
-struct Record ReadRecord(struct Record *records, int index) {
-	struct Record newRecord;
-	// ToDo cin 
-	return newRecord;
-}
-
-
-struct Record UpdateRecord(struct Record inRecord, int index) { 
-	return inRecord;
-}
-
-void deleteRecord(struct Record inRecord, int index) {
-	
+	DrawLine();
+	cout << "| Отдел кадров" << GetSpacebar(80 - sizeof("| Отдел кадров")) << "|\n";
+	DrawLine();
+	//|Фамилия |Инициалы |Год рожд | Оклад|
+	cout << "| Фамилия" << GetSpacebar(21 - sizeof("| Фамилия"));
+	cout << "| Инициалы" << GetSpacebar(21 - sizeof("| Инициалы"));
+	cout << "| Год рожд" << GetSpacebar(21 - sizeof("| Год рожд"));
+	cout << "| Оклад" << GetSpacebar(20 - sizeof("| Оклад")) << "|\n";
+	DrawLine();
+	for (int i = 0; i < 3; i++) {
+		cout << "| " << records[i].surName << GetSpacebar(18 - GetSize(records[i].surName));
+		cout << "| " << records[i].ident << GetSpacebar(18 - GetSize(records[i].ident));
+		cout << "| " << records[i].year << GetSpacebar(18 - to_string(records[i].year).size());
+		cout << "| " << records[i].salary << GetSpacebar(22 - to_string(records[i].salary).size()) << "|\n";
+		DrawLine();
+	}
+	cout << "| Примечание: оклад установлен по состоянию на 1 января 2000 года" << GetSpacebar(80 - sizeof("| Примечание: оклад установлен по состоянию на 1 января 2000 года")) << "|\n";
+	DrawLine();
 }
 
 int main()
@@ -68,31 +90,12 @@ int main()
 	SetConsoleOutputCP(1251);
 
 
-	struct Record Records[10];
-
-	Records[0] = { "Русаков", "И. И.", 1975, 517.50 };
-	Records[1] = { "Русаков", "П.П.", 1956, 219.10 };
-	Records[2] = { "Русаков", "М.С.", 1967, 300.10 };
-	
-	Rec
+	struct Record records[10];
 
 
-	DrawLine();
-	cout << "| Отдел кадров" <<  GetSpacebar(80-sizeof("| Отдел кадров")) << "|\n";
-	DrawLine();
-	//|Фамилия |Инициалы |Год рожд | Оклад|
-	cout << "| Фамилия" << GetSpacebar(21 - sizeof("| Фамилия")) ;
-	cout << "| Инициалы" << GetSpacebar(21 - sizeof("| Инициалы"));
-	cout << "| Год рожд" << GetSpacebar(21 - sizeof("| Год рожд"));
-	cout << "| Оклад" << GetSpacebar(20 - sizeof("| Оклад")) << "|\n";
-	DrawLine();
-	for (int i=0; i<3; i++){
-		cout << "| " << lines[i].surName << GetSpacebar(18 - GetSize(lines[i].surName));
-		cout << "| " << lines[i].ident << GetSpacebar(18 - GetSize(lines[i].ident));
-		cout << "| " << lines[i].year << GetSpacebar(18 - to_string(lines[i].year).size());
-		cout << "| " << lines[i].salary << GetSpacebar(22 - to_string(lines[i].salary).size()) << "|\n";
-		DrawLine();
-	}
-	cout << "| Примечание: оклад установлен по состоянию на 1 января 2000 года" << GetSpacebar(80 - sizeof("| Примечание: оклад установлен по состоянию на 1 января 2000 года")) << "|\n";
-	DrawLine();
+	records[0] = { "Русаков", "И. И.", 1975, 517.50, true };
+	records[1] = { "Русаков", "П.П.", 1956, 219.10, true };
+	records[2] = { "Русаков", "М.С.", 1967, 300.10, true };
+
+	Draw(records);
 }

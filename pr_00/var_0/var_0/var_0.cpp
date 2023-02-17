1283e6b67779bd3b1ef9f9c85d8c447926298799
Aleksey <rusal@bk.ru>
2023-02-17 09:43:51 +0300

    fix

--- pr_00/var_0/var_0/var_0.cpp
+++ pr_00/var_0/var_0/var_0.cpp
@@ -5,13 +5,20 @@
 
 using namespace std;
 
+struct Date {
+	unsigned short day;
+	unsigned short month;
+	unsigned short year;
+};
+
 struct Record
 {
 	char surName[17];
 	char ident[12];
 	unsigned short year;
 	float salary;
-	bool isEmpty;
+	struct Date date;
+	//bool isEmpty;
 };
 
 
@@ -28,9 +35,14 @@ struct Record ReadRecord(struct Record *records, int index) {
 	return newRecord;
 }
 
-
-struct Record UpdateRecord(struct Record inRecord, int index) { 
-	return inRecord;
+/// <summary>
+/// Функция которая обновляет запись
+/// </summary>
+/// <param name="inRecord">массив записей</param>
+/// <param name="index">индекс который надо обновить</param>
+/// <returns>Результат операции</returns>
+bool UpdateRecord(struct Record inRecord, int index) { 
+	return false;
 }
 
 void deleteRecord(struct Record inRecord, int index) {
@@ -62,8 +74,7 @@ int GetSize(char* msg) {
 	return size;
 }
 
-void Draw(struct Record * records) {
-
+void DrawWrong(struct Record * records) {
 	DrawLine();
 	cout << "| Отдел кадров" << GetSpacebar(80 - sizeof("| Отдел кадров")) << "|\n";
 	DrawLine();
@@ -78,10 +89,24 @@ void Draw(struct Record * records) {
 		cout << "| " << records[i].ident << GetSpacebar(18 - GetSize(records[i].ident));
 		cout << "| " << records[i].year << GetSpacebar(18 - to_string(records[i].year).size());
 		cout << "| " << records[i].salary << GetSpacebar(22 - to_string(records[i].salary).size()) << "|\n";
+		//cout << "| " << records[i].salary << GetSpacebar(22 - to_string(records[i].salary).size()) << "|\n";
 		DrawLine();
 	}
 	cout << "| Примечание: оклад установлен по состоянию на 1 января 2000 года" << GetSpacebar(80 - sizeof("| Примечание: оклад установлен по состоянию на 1 января 2000 года")) << "|\n";
 	DrawLine();
+}
+
+void Draw(struct Record* records) {
+	cout << endl;	cout.width(79); cout.fill('-'); cout << "-" << endl;
+	cout.fill(' '); cout.width(78);  cout << left << "| Отдел кадров"; cout << "|" << endl;
+	cout.width(79); cout.fill('-'); cout << "-" << endl;
+	cout.fill(' ');
+	cout.width(17); cout << "| Фамилия";
+	cout.width(12); cout << "| Инициалы";
+	cout.width(12); cout << "| Год рожд" ;
+	cout.width(12); cout << "| Оклад";
+	cout.width(25); cout << "| Дата приема на работу";
+	cout <<"|" << endl;
 }
 
 int main()
@@ -91,9 +116,11 @@ int main()
 
 	struct Record records[10];
 
-	records[0] = { "Иванов", "И. И.", 1975, 517.50, true };
-	records[1] = { "Петренко", "П.П.", 1956, 219.10, true };
-	records[2] = { "Панковский", "М.С.", 1967, 300.10, true };
-
+	records[0] = { "Иванов", "И. И.", 1975, 517.50, {01,02,2010} };
+	records[1] = { "Петренко", "П.П.", 1956, 219.10, {02,03,2020} };
+	records[2] = { "Панковский", "М.С.", 1967, 300.10, {12,12,2012} };
+		
 	Draw(records);
+	//cout << endl;
+	//DrawWrong(records);
 }

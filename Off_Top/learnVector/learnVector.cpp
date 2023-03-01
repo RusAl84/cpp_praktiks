fca4398dcf5398ac982c9821db21dd7abdba9a3f
Aleksey <rusal@bk.ru>
2023-03-01 14:02:35 +0300

    add myList

--- Off_Top/learnVector/learnVector.cpp
+++ Off_Top/learnVector/learnVector.cpp
@@ -2,27 +2,36 @@
 #include <vector>
 using namespace std;
 
+void printVect(vector<int> vect1) {
+    for (int i = 0; i < vect1.size(); i++)
+        cout << vect1[i] << " ";
+}
 
 int main()
 {
-    struct bigStruct {
-        char name1[10000];
-        char name2[10000];
-        char name3[10000];
-        char name4[10000];
-        char name5[10000];
-        char name6[10000];
-        char name7[10000];
-        char name8[10000];
+    vector <int> vect1 = { 1,2,3,4,5 };
+    printVect(vect1);
+    vect1.insert(vect1.begin() + 2, 100);
+    cout << endl;
+    printVect(vect1);
+    //struct bigStruct {
+    //    char name1[10000];
+    //    char name2[10000];
+    //    char name3[10000];
+    //    char name4[10000];
+    //    char name5[10000];
+    //    char name6[10000];
+    //    char name7[10000];
+    //    char name8[10000];
 
-    };
-    struct bigStruct s2;
-    vector<bigStruct> s1;
-    vector<bigStruct> s3;
-    vector<bigStruct> s4;
-    for (int i=0;i++;i<10000){
-         s1.insert(s1.begin(), s2);
-         s3.insert(s1.begin(), s2);
-         s4.insert(s1.begin(), s2);
-    }
+    //};
+    //struct bigStruct s2;
+    //vector<bigStruct> s1;
+    //vector<bigStruct> s3;
+    //vector<bigStruct> s4;
+    //for (int i=0;i++;i<10000){
+    //     s1.insert(s1.begin(), s2);
+    //     s3.insert(s1.begin(), s2);
+    //     s4.insert(s1.begin(), s2);
+    //}
 }
---
+++ Off_Top/myList/myList.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+struct node {
+    int data;
+    struct node* next;
+};
+struct node* myHead;
+int countItem = 0;
+
+void addItem(int data) {
+    struct node* newItem = new node();
+    newItem->data = data;
+    if (countItem == 0) {
+        newItem->next = NULL;    
+    }
+    else
+    {
+        newItem->next = myHead;
+    }
+    myHead = newItem;
+    countItem++;
+}
+
+void editItem(int index, int data) {
+    if (index >= 0 and index < countItem) {
+        struct node* current = myHead;
+        for (int i = 0; i < index;  i++) {
+            current = current->next;
+            //cout << "+" << current->data;
+        }
+        current->data = data;
+    }
+    else {
+        cout << "Ошибка индекс не в диапазоне";
+    }
+
+}
+
+void printMyList() {
+    struct node* current = myHead;
+    cout << endl;
+    while (current) {
+        cout << " " << current->data;
+        current = current->next;
+    }
+}
+
+int main()
+{
+    cout << "myList\n";
+
+    addItem(1);
+    addItem(2);
+    addItem(3);
+    addItem(4);
+    printMyList();
+    editItem(2, 8);
+    printMyList();
+}

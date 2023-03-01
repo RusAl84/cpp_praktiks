a0150ac0993e227d813a1d033573390b494f3e2e
Aleksey <rusal@bk.ru>
2023-03-01 09:47:08 +0300

    add pr2

---
+++ Off_Top/OutOfMemory/OutOfMemory.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main()
+{
+    int size = 1e+20;
+    int csize = 1e+40;
+    for (int i = 0; i < 1e+10; i++){
+        std::cout << i << std::endl;
+        /*calloc(size, csize);*/
+       int * p =  new int(csize);
+    }
+}
---
+++ Off_Top/learnVector/learnVector.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    struct bigStruct {
+        char name1[10000];
+        char name2[10000];
+        char name3[10000];
+        char name4[10000];
+        char name5[10000];
+        char name6[10000];
+        char name7[10000];
+        char name8[10000];
+
+    };
+    struct bigStruct s2;
+    vector<bigStruct> s1;
+    vector<bigStruct> s3;
+    vector<bigStruct> s4;
+    for (int i=0;i++;i<10000){
+         s1.insert(s1.begin(), s2);
+         s3.insert(s1.begin(), s2);
+         s4.insert(s1.begin(), s2);
+    }
+}

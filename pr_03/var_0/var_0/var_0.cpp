d387f0e4fe683bc669696013215b4f89fff09b19
Aleksey <rusal@bk.ru>
2023-03-10 13:28:03 +0300

    add d list

--- pr_03/var_0/var_0/var_0.cpp
+++ pr_03/var_0/var_0/var_0.cpp
@@ -21,52 +21,13 @@ struct Record
 	//bool isEmpty;
 };
 
+struct node {
+	Record data;
+	struct node* next;
+};
 
-void DrawLine() {
-	for (int i = 0; i < 80; i++) {
-		cout << "-";
-	}
-	cout << "\n";
-}
-
-char* GetSpacebar(int count) {
-	char* msg = new char[count];
-	for (int i = 0; i < count; i++)
-	{
-		msg[i] = ' ';
-	}
-	msg[count] = '\0';
-	return msg;
-}
-
-int GetSize(char* msg) {
-	int size = 0;
-	while (msg[size] != '\0')
-		size++;
-	return size;
-}
-
-void DrawWrong(struct Record * records) {
-	DrawLine();
-	cout << "| Отдел кадров" << GetSpacebar(80 - sizeof("| Отдел кадров")) << "|\n";
-	DrawLine();
-	//|Фамилия |Инициалы |Год рожд | Оклад|
-	cout << "| Фамилия" << GetSpacebar(21 - sizeof("| Фамилия"));
-	cout << "| Инициалы" << GetSpacebar(21 - sizeof("| Инициалы"));
-	cout << "| Год рожд" << GetSpacebar(21 - sizeof("| Год рожд"));
-	cout << "| Оклад" << GetSpacebar(20 - sizeof("| Оклад")) << "|\n";
-	DrawLine();
-	for (int i = 0; i < 3; i++) {
-		cout << "| " << records[i].surName << GetSpacebar(18 - GetSize(records[i].surName));
-		cout << "| " << records[i].ident << GetSpacebar(18 - GetSize(records[i].ident));
-		cout << "| " << records[i].year << GetSpacebar(18 - to_string(records[i].year).size());
-		cout << "| " << records[i].salary << GetSpacebar(22 - to_string(records[i].salary).size()) << "|\n";
-		//cout << "| " << records[i].salary << GetSpacebar(22 - to_string(records[i].salary).size()) << "|\n";
-		DrawLine();
-	}
-	cout << "| Примечание: оклад установлен по состоянию на 1 января 2000 года" << GetSpacebar(80 - sizeof("| Примечание: оклад установлен по состоянию на 1 января 2000 года")) << "|\n";
-	DrawLine();
-}
+struct node* myHead;
+int countItem = 0;
 
 void myCentr(string s, int wLine) {
 
@@ -187,6 +148,137 @@ int  findIndex(struct Record* records, const struct Date date) {
 	return ind;
 };
 
+/// <summary>
+/// Добавляет элемент в начало.
+/// </summary>
+/// <param name="data"> данные элемента</param>
+void addItem(Record data)
+{
+	struct node* newItem = new node();
+	newItem->data = data;
+	if (countItem == 0) {
+		newItem->next = NULL;
+	}
+	else
+	{
+		newItem->next = myHead;
+	}
+	myHead = newItem;
+	countItem++;
+}
+/// <summary>
+/// Вставка элемента в список
+/// </summary>
+/// <param name="index">индекс после которого вставить</param>
+/// <param name="data">значение которое необходимо вставить</param>
+void insertItem(int index, Record data) {
+	if (not (index >= 0 and index <= countItem and countItem >= 0))
+		return;
+	if (index == 0) {
+		addItem(data);
+	}
+	else {
+		struct node* current = myHead;
+		for (int i = 0; i < index - 1; i++) {
+			current = current->next;
+		}
+		struct node* newItem = new node();
+		newItem->data = data;
+		newItem->next = current->next;
+		current->next = newItem;
+	}
+}
+/// <summary>
+/// Изменить элемент списка
+/// </summary>
+/// <param name="index">индекс изменяемого элемента</param>
+/// <param name="data">значение на которое нужно изменить</param>
+void editItem(int index, Record data) {
+	if (index >= 0 and index < countItem and countItem>0) {
+		struct node* current = myHead;
+		for (int i = 0; i < index; i++) {
+			current = current->next;
+			//cout << "+" << current->data;
+		}
+		current->data = data;
+	}
+	else {
+		cout << endl << "Ошибка индекс не в диапазоне";
+	}
+}
+/// <summary>
+/// Функция для удаления элемента из динамического списка
+/// </summary>
+/// <param name="index">индекс удаляемого элемента</param>
+void deleteItem(int index) {
+	if (index >= 0 and index < countItem and countItem>0) {
+		struct node* current = myHead;
+		struct node* old;
+		if (index == 0) {
+			old = myHead;
+			myHead = current->next;
+			delete old;
+			countItem--;
+		}
+		else {
+			int i = 0;
+			while (current) {
+				if (i == index - 1) {
+					old = current->next->next;
+					delete current->next;
+					current->next = old;
+					countItem--;
+					break;
+				}
+				i++;
+				current = current->next;
+			}
+		}
+	}
+
+}
+void printMyList() {
+	struct node* current = myHead;
+	cout << endl;
+	while (current) {
+		cout << " " << current->data.surName;
+		current = current->next;
+	}
+}
+
+void DrawWithList() {
+	cout << endl;	cout.width(79); cout.fill('-'); cout << "-" << endl;
+	cout.fill(' '); cout.width(78);  cout << left << "|Отдел кадров"; cout << "|" << endl;
+	cout.width(79); cout.fill('-'); cout << "-" << endl;
+	cout.fill(' ');
+
+	cout << left << "|"; myCentr("Фамилия", 16);
+	cout << left << "|"; myCentr("Инициалы", 11);
+	cout << left << "|"; myCentr("Год рожд", 10);
+	cout << left << "|"; myCentr("Оклад", 10);
+	cout << left << " |"; myCentr("Дата приема на работу", 24);
+	cout << "|" << endl;
+	cout.width(79); cout.fill('-'); cout << "-" << endl;
+	cout.fill(' ');
+	struct node* current = myHead;
+	while (current) {
+		cout << left << "|"; cout.width(16); cout << left << current->data.surName;
+		cout << left << "|"; cout.width(11); cout << left << current->data.ident;
+		cout << left << "|+"; cout.width(10); cout << left << current->data.year;
+		std::cout.precision(2);
+		cout << left << "|"; cout.width(11); cout << left << fixed << current->data.salary;
+		cout << left << "|";
+		printDate(current->data.date.day, current->data.date.month, current->data.date.year, 26);
+		cout << "|" << endl;
+		current = current->next;
+	}
+	cout.width(79); cout.fill('-'); cout << "-" << endl;
+	cout.fill(' '); cout.width(78);  cout << left << "|Примечание: оклад установлен по состоянию на 1 января 2000 года"; cout << "|" << endl;
+	cout.width(79); cout.fill('-'); cout << "-" << endl;
+}
+
+
+
 int main()
 {
 	SetConsoleCP(1251);
@@ -194,9 +286,9 @@ int main()
 
 	struct Record records[10];
 
-	records[0] = { "Иванов", "И.И.", 1975, 517.50, {01,02,2010} };      // Свой комментарий
-	records[1] = { "Петренко", "П.П.", 1956, 219.10, {02,03,2020} };      // Свой комментарий
-	records[2] = { "Панковский", "М.С.", 1967, 300.10, {12,12,2012} };      // Свой комментарий
+	records[0] = { "Иванов", "И.И.", 1975, 517.50, {01,02,2010} };      
+	records[1] = { "Петренко", "П.П.", 1956, 219.10, {02,03,2020} };      
+	records[2] = { "Панковский", "М.С.", 1967, 300.10, {12,12,2012} };      
 	cout << "Исходные данные:" << endl;
 	Draw(records);
 
@@ -227,4 +319,16 @@ int main()
 	}
 	cout << "Данные по варианту задания:" << endl;
 	Draw(newRecords);
+
+    // Практика 3 Динамический список
+	//system("mspaint");
+	addItem(records[0]);
+	addItem(records[1]);
+	addItem(records[2]);
+	DrawWithList();
+	struct Record newRecord = { "Русаков", "А.М.", 1984, 712.50, {01,02,2023} };
+	insertItem(1, newRecord);
+	DrawWithList();
+	deleteItem(2);
+	DrawWithList();
 }

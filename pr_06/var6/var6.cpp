f8d63abed4195fbbcbb1c532e883e06d0646ef17
Aleksey <rusal@bk.ru>
2023-04-27 21:48:58 +0300

    fix

--- pr_06/var6/var6.cpp
+++ pr_06/var6/var6.cpp
@@ -4,12 +4,11 @@ class A
 {public: void f() { cout << " A";}};
 class B: public virtual A
 {public: void f() { cout << " B";}};
-
 class C: public virtual A
 {public: void f() { cout << " C";}};
 class E : public virtual A
 { public: void f() { cout << " E"; }};
-class D : public  B, public  C, public  E
+class D : public virtual B, public virtual C, public virtual E
 {public: void f() { cout << " D"; }};
 class F: public virtual D
 {  /*public: void f() { cout << " F"; };*/
@@ -17,6 +16,7 @@ class F: public virtual D
 int main()
 {
     F object;
+    object.f();
     object.A::f();
     object.B::f();
     object.C::f();
@@ -33,6 +33,20 @@ int main()
 [16:08, 27.04.2023] Мерсов: Классы А Е С должны при наследовании быть виртуальными
 [16:08, 27.04.2023] Rusakov Aleksey: Благодарю
 [16:10, 27.04.2023] Мерсов: class D: public B, public virtual C, public virtual E; - для примера  например твоего и соответственно надо посмоьреть остальное*/
+
+/*
+https://ru.wikipedia.org/wiki/Microsoft_Foundation_Classes
+https://ru.wikipedia.org/wiki/Windows_API#/media/%D0%A4%D0%B0%D0%B9%D0%BB:WinAPI_Scheme.svg
+https://vk.com/wall-54530371_254
+https://github.com/RusAl84/cpp-cheatsheet
+https://quickref.me/cpp
+https://www3.ntu.edu.sg/home/ehchua/programming/cpp/cp3_OOP.html
+Виртуальный означает видимый, но не существующий в реальности.
+если из класса выходят более чем 1 связь тоэтот класс должен быть виртуальным!!!
+Об'явления классов дать в следующем порядке как на схеме.
+*/
+
+
 
 
 //metod example

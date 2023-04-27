b98dbf13d4f37c26deb0903ccacdd64122f72f22
Aleksey <rusal@bk.ru>
2023-04-27 13:38:56 +0300

    fix

--- pr_06/var6/var6.cpp
+++ pr_06/var6/var6.cpp
@@ -1,25 +1,40 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//using namespace std;
+//class A
+//{public: void f() { cout << " A";}};
+//class B: public virtual A
+//{public: void f() { cout << " B";}};
+//class D: public virtual A, public virtual B
+//{public: void f() { cout << " D";}};
+//class C: public virtual B
+//{public: void f() { cout << " C";}};
+//class E :public virtual C
+//{public: void f() { cout << " E"; } };
+//class F: public D, public E
+//{  /*public: void f() { cout << " F"; };*/
+//};
+//int main()
+//{
+//    F object;
+//    object.A::f();
+//    object.B::f();
+//    object.C::f();
+//    object.D::f();
+//    object.E::f();
+//}
+
+#include <iostream>
 using namespace std;
 class A
-{public: void f() { cout << " A"; };
-class B : public virtual A
-{public: void f() { cout << " B"; };
-class D : public virtual A, public virtual B
-{/*public: void f() { cout << "D";};
-                class
-                C : public virtual B
-                {
-                public : void f() { cout <<<<" C"; }
-                class
-                E : public C
-                {
-                public : void f() { cout <<<<" E";
-                class
-                F : public D , public E
-                { ;
-                ;// public: void f() { cout << "F"; };
-                };
+{public: void f() { cout << " A";}};
+class B: public virtual A
+{public: void f() { cout << " B";}};
+class C: public virtual B
+{//public: void f() { cout << " C";}
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    C object;
+    object.f();
+
 }

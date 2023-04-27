e2987c86c51fa42926b9809ec4be2df36a40f377
Aleksey <rusal@bk.ru>
2023-04-27 21:32:52 +0300

    fix

--- pr_06/var6/var6.cpp
+++ pr_06/var6/var6.cpp
@@ -4,13 +4,14 @@ class A
 {public: void f() { cout << " A";}};
 class B: public virtual A
 {public: void f() { cout << " B";}};
-class D: public virtual B, public virtual C, public virtual E
-{public: void f() { cout << " D";}};
+
 class C: public virtual A
 {public: void f() { cout << " C";}};
-class E: public virtual A, public virtual C
-{public: void f() { cout << " E"; } };
-class F: public virtual D, public virtual E
+class E : public virtual A
+{ public: void f() { cout << " E"; }};
+class D : public  B, public  C, public  E
+{public: void f() { cout << " D"; }};
+class F: public virtual D
 {  /*public: void f() { cout << " F"; };*/
 };
 int main()
@@ -22,6 +23,16 @@ int main()
     object.D::f();
     object.E::f();
 }
+
+/*
+[16:02, 27.04.2023] Мерсов: Об'явления классов дать в следующем порядке:А, В,С,Е,D,F
+[16:02, 27.04.2023] Мерсов: У тебя идет D,E
+[16:04, 27.04.2023] Мерсов: У тебя D производный от Е об'явлено а класс Е еще не об'явлен
+[16:04, 27.04.2023] Мерсов: Поэтому и говорит об ошибке
+[16:07, 27.04.2023] Мерсов: Еще ошибки есть следующие: если из класса выходят более чем 1 связь тоэтот класс должен быть виртуальным!!!
+[16:08, 27.04.2023] Мерсов: Классы А Е С должны при наследовании быть виртуальными
+[16:08, 27.04.2023] Rusakov Aleksey: Благодарю
+[16:10, 27.04.2023] Мерсов: class D: public B, public virtual C, public virtual E; - для примера  например твоего и соответственно надо посмоьреть остальное*/
 
 
 //metod example

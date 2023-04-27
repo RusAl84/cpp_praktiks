a823b90dae05ce7e0c9055320a628e4d16490155
Aleksey <rusal@bk.ru>
2023-04-27 14:00:09 +0300

    fix

--- pr_06/var6/var6.cpp
+++ pr_06/var6/var6.cpp
@@ -1,4 +1,31 @@
-﻿//#include <iostream>
+﻿#include <iostream>
+using namespace std;
+class A
+{public: void f() { cout << " A";}};
+class B: public virtual A
+{public: void f() { cout << " B";}};
+class D: public virtual B, public virtual C, public virtual E
+{public: void f() { cout << " D";}};
+class C: public virtual A
+{public: void f() { cout << " C";}};
+class E: public virtual A, public virtual C
+{public: void f() { cout << " E"; } };
+class F: public virtual D, public virtual E
+{  /*public: void f() { cout << " F"; };*/
+};
+int main()
+{
+    F object;
+    object.A::f();
+    object.B::f();
+    object.C::f();
+    object.D::f();
+    object.E::f();
+}
+
+
+//metod example
+//#include <iostream>
 //using namespace std;
 //class A
 //{public: void f() { cout << " A";}};
@@ -23,18 +50,20 @@
 //    object.E::f();
 //}
 
-#include <iostream>
-using namespace std;
-class A
-{public: void f() { cout << " A";}};
-class B: public virtual A
-{public: void f() { cout << " B";}};
-class C: public virtual B
-{//public: void f() { cout << " C";}
-};
-int main()
-{
-    C object;
-    object.f();
 
-}
+//Rusakov task
+//#include <iostream>
+//using namespace std;
+//class A
+//{public: void f() { cout << " A";}};
+//class B: public virtual A
+//{public: void f() { cout << " B";}};
+//class C: public virtual B
+//{//public: void f() { cout << " C";}
+//};
+//int main()
+//{
+//    C object;
+//    object.f();
+//
+//}

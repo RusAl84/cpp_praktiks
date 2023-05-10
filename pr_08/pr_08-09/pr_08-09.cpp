bca3c287da8e502c7aedb9e721bea61986c5be03
Aleksey <rusal@bk.ru>
2023-05-10 09:38:29 +0300

    fix folder adn start 8

---
+++ pr_08/pr_08-09/pr_08-09.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum class sex : char { Women, Men, Any };  
+
+class clStudent {
+protected:
+    string surName;     //Фамилия(string);
+    string name;        //Имя(string);
+    string middleName;  //Отчество(string);
+    string bDate;       //Дата рождения(clDate);
+    enum class sex;     //Пол;
+    string startDate;   //Дата зачисления(clDate)
+    double studentship; //Стипендия;
+    
+    
+
+};
+
+class clGroup {
+protected:
+    string name; // – Название (шифр) группы (строка, string);
+    string institute; // – Институт(строка, string);
+    string department; // – Выпускающая кафедра(строка, string);
+    string tutorName; // – Фамилия ИО куратора(строка, string);
+    int countStud; // –	Численность(целое);
+    clStudent *listOfStudents; // – список студентов, представляющий собой динамический массив объектов «Студент» (clStudent), 
+public:
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}

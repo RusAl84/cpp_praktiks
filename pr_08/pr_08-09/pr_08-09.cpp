0047af3707fbe6b71e4ae7bfb5e5d35a0f803e20
Aleksey <rusal@bk.ru>
2023-05-10 10:04:22 +0300

    fix

--- pr_08/pr_08-09/pr_08-09.cpp
+++ pr_08/pr_08-09/pr_08-09.cpp
@@ -15,7 +15,22 @@ protected:
     clDate startDate;   //Дата зачисления(clDate)
     double studentship; //Стипендия;
 public:
-    clStudent() {};
+    clStudent(string _surName,
+        string _name,
+        string _middleName,
+        clDate _bDate,
+        enum class sex  _sex,
+        clDate _startDate,
+        double _studentship) 
+    {
+        surName = _surName;
+        name = _name;
+        middleName = _middleName;
+        bDate = _bDate;
+        sex = _sex;
+        startDate = _startDate;
+        studentship = _studentship;
+    };
 };
 
 class clGroup {
@@ -27,13 +42,27 @@ protected:
     int countStud; // –	Численность(целое);
     clStudent *listOfStudents; // – список студентов, представляющий собой динамический массив объектов «Студент» (clStudent), 
 public:
-    clGroup() { listOfStudents == NULL; };
-    void addStudent(clStudent* st) {
-    
+    clGroup() { 
+        countStud = 0;
+        listOfStudents == NULL; };
+    void addStudent(string _surName,
+        string _name,
+        string _middleName,
+        clDate _bDate,
+        enum class sex _sex,
+        clDate _startDate,
+        double _studentship) {
+        if (countStud==0) {
+            listOfStudents =  new clStudent(_surName, _name, _middleName, _bDate, _sex, _startDate , _studentship);
+            countStud++;
+        }
+
     };
 };
 
 int main()
 {
+    clGroup BBBO01;
+    BBBO01.addStudent("Филатов", "Никита", "Михайлович", { 12, 05, 1974 }, sex::Men, { 01, 08, 1996 }, 1000);
     std::cout << "Hello World!\n";
 }

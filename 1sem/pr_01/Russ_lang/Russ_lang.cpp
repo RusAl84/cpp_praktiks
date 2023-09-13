bb726eefa7e8da276ed2b7fd4245826963d0d531
Aleksey <rusal@bk.ru>
2023-09-13 20:45:50 +0300

    russ lang

---
+++ 1sem/pr_01/Russ_lang/Russ_lang.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+    SetConsoleOutputCP(1251);
+    cout << "Русский язык\n";
+}

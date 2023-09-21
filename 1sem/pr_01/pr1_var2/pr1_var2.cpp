feabf195405c6d74def35f093e732927aa01158c
Aleksey <rusal@bk.ru>
2023-09-21 11:47:44 +0300

    fix pr1

---
+++ 1sem/pr_01/pr1_var2/pr1_var2.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+    SetConsoleOutputCP(1251);
+
+    //Процессор Частота (Mgz) RAM (Mb) Тип
+
+    string cpu1 = "Pentium - III";
+    string cpu2 = "AMD-К6";
+    string cpu3 = "PowerPC";
+    int freq1 = 233;
+    int freq2 = 166;
+    int freq3 = 620;
+    int ram1 = 128;
+    int ram2 = 128;
+    int ram3 = 128;
+    char type1 = 'C';
+    char type2 = 'C';
+    char type3 = 'R';
+    //
+    //
+
+    int wigth = 80;
+    printf("\n");
+    for (int i = 0; i < wigth; i++)
+        printf("-");
+    printf("\n");
+    printf("|Характеристики ПЭВМ                                                           |");    
+    printf("\n");
+    for (int i = 0; i < wigth; i++)
+        printf("-");
+    printf("\n");
+    printf("|Процессор             | Частота(Mgz)          |  RAM(Mb)       |     Тип      |");
+    printf("\n");
+    for (int i = 0; i < wigth; i++)
+        printf("-");
+    printf("\n");
+    
+    //строка 1
+    printf("|%s", cpu1.c_str());
+    for (int i = 0; i < 22 - cpu1.size(); i++)
+        printf(" ");
+    printf("|%d", freq1);
+    for (int i = 0; i < 20; i++)
+        printf(" ");
+    printf("|%d", ram1);
+    for (int i = 0; i < 13 ; i++)
+        printf(" ");
+    printf("|%c", type1);
+    for (int i = 0; i < 13; i++)
+        printf(" ");
+    printf("|\n");
+    for (int i = 0; i < wigth; i++)
+        printf("-");
+    printf("\n");
+
+
+    //строка 2
+    printf("|%s", cpu2.c_str());
+    for (int i = 0; i < 22 - cpu2.size(); i++)
+        printf(" ");
+    printf("|%d", freq2);
+    for (int i = 0; i < 20; i++)
+        printf(" ");
+    printf("|%d", ram2);
+    for (int i = 0; i < 13; i++)
+        printf(" ");
+    printf("|%c", type2);
+    for (int i = 0; i < 13; i++)
+        printf(" ");
+    printf("|\n");
+    for (int i = 0; i < wigth; i++)
+        printf("-");
+    printf("\n");
+
+    //строка 3
+    printf("|%s", cpu3.c_str());
+    for (int i = 0; i < 22 - cpu3.size(); i++)
+        printf(" ");
+    printf("|%d", freq3);
+    for (int i = 0; i < 20; i++)
+        printf(" ");
+    printf("|%d", ram3);
+    for (int i = 0; i < 13; i++)
+        printf(" ");
+    printf("|%c", type3);
+    for (int i = 0; i < 13; i++)
+        printf(" ");
+    printf("|\n");
+    for (int i = 0; i < wigth; i++)
+        printf("-");
+    printf("\n");
+
+    printf("|Тип: C - CISC-процессор, R - RISC-процессор                                   |");
+    printf("\n");
+    for (int i = 0; i < wigth; i++)
+        printf("-");
+    printf("\n");
+
+    return 0;
+}

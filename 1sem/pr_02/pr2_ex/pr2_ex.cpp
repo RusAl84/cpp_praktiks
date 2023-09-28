a3247f66e4ac2c793cf60f27fa3ccf5aefefc48f
Aleksey <rusal@bk.ru>
2023-09-28 09:44:16 +0300

    fix

--- 1sem/pr_02/pr2_ex/pr2_ex.cpp
+++ 1sem/pr_02/pr2_ex/pr2_ex.cpp
@@ -2,10 +2,14 @@
 
 #include <stdio.h>
 #include <math.h>
-
+#include <Windows.h>
+#include <iostream>
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+    SetConsoleOutputCP(1251);
     double x, y;
     double a = 12.5, b = 1.3;
     double c = 14.1, d = 2.7;

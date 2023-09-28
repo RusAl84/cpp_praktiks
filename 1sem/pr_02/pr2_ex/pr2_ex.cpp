6b1359e13c02a9aa6ec9640469c1d5202957d8e5
Aleksey <rusal@bk.ru>
2023-09-28 09:38:25 +0300

    add pr_2

---
+++ 1sem/pr_02/pr2_ex/pr2_ex.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    double x, y;
+    double a = 12.5, b = 1.3;
+    double c = 14.1, d = 2.7;
+    double t1, t2;
+    double ax;
+    printf("Введите x, y >");
+    
+    scanf("%lf %lf", &x, &y);
+}

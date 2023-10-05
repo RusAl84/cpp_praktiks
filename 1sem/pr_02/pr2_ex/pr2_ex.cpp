7fe80a767cc8e1cfb281c64ef49ccf26577cb0e6
Aleksey <rusal@bk.ru>
2023-10-05 09:33:29 +0300

    pr_2 sfix

--- 1sem/pr_02/pr2_ex/pr2_ex.cpp
+++ 1sem/pr_02/pr2_ex/pr2_ex.cpp
@@ -12,16 +12,21 @@ int main()
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
     SetConsoleOutputCP(1251);
-    double x, y;
+    double x = 1, y = 1;
     double a = 12.5, b = 1.3;
     double c = 14.1, d = 2.7;
     double t1, t2;
     double ax;
     printf("Введите x, y >");
     
-    scanf("%lf %lf", &x, &y);
-
-    t1 = x;
+    //scanf("%lf %lf", &x, &y);
+    
+    double t1_p1 = 1 / (pow(b,3));
+    // todo деление на ноль
+    double t1_p2 = log(y / x);
+    double t1_p3 = (pow(a, 2) * pow(x, 2)) / (2 * pow(y, 2));
+    double t1_p4 = t1_p2 - t1_p3;
+    t1 = t1_p1 * t1_p4;
     t2 = y;
 
     printf("t1 = %lg\n", t1); printf("t2 = %lg\n", t2);

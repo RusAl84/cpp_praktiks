8b65d0049b4b3cae7f71ef470bf3b68ff11f0498
Aleksey <rusal@bk.ru>
2023-10-12 10:28:47 +0300

    add ex

---
+++ 1sem/pr_03/var03/var03.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+    SetConsoleOutputCP(1251);
+    int ch = 'M';
+    //1 - +
+    //2 - -
+    //3 - *
+    //4 – все остальные
+    switch (ch) {
+    case '+': 
+        printf("1\n");
+    default:
+        printf("4\n");
+    }
+
+
+    return 0;
+}

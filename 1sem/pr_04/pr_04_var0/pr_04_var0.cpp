821f24ed146750d85902528f5b909c4cd6bc96da
Aleksey <rusal@bk.ru>
2023-10-26 10:30:41 +0300

    fix

--- 1sem/pr_04/pr_04_var0/pr_04_var0.cpp
+++ 1sem/pr_04/pr_04_var0/pr_04_var0.cpp
@@ -11,6 +11,33 @@ int main()
     SetConsoleOutputCP(1251);
     double x = 0;
     double y = 0;
-    printf("Точка %f");
+    
+    bool getIntoArea = false;
+    //проверка для четверти I
+    if ((x >= 0) && (y >= 0)) {
+        if (y <= (1 - x)) {
+            getIntoArea = true; //1
+        }
+        if ( (( (pow(y,2) + pow(x, 2)) >= 1) )  && ((x <= 1) && (y <= 1)) ) {
+            getIntoArea = true; //2
+        }
+    }
+    //проверка для четверти II
+    if ((x >= 0) && (y <= 0)) {
+
+    }
+    //проверка для четверти III
+    if ((x <= 0) && (y <= 0)) {
+
+    }
+    //проверка для четверти IV
+    if ((x <= 0) && (y >= 0)) {
+
+    }
+
+    if (getIntoArea) {
+        printf("Точка x = %f  y = %f \n", x, y);
+        printf("попадает в заштрихованную область");
+    }
     return 0;
 }

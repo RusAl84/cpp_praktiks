afb7b99f6b17e738577d7a7280737e726105f323
Aleksey <rusal@bk.ru>
2023-10-26 10:16:52 +0300

    start pract 4

--- 1sem/pr_03/ex_switch/ex_switch.cpp
+++ 1sem/pr_03/ex_switch/ex_switch.cpp
@@ -7,26 +7,26 @@ int main()
 	printf("Pozdravlyau!!! \n");
 	switch (k)
 	{
-	case 6: 
-		printf("U vas 6-ka\n "); break;
-	case 7: 
-		printf("U vas 7-ka\n "); break;
-	case 8: 
-		printf("U vas 8 - ka\n "); break;
-	case 9: 
-		printf("U vas 9-ka\n "); break;
-	case 10: 
-		printf("U vas 10-ka\n "); break;
-	case 11: 
-		printf("U vas Valet\n "); break;
-	case 12: 
-		printf("U vas Dama\n "); break;
-	case 13: 
-		printf("U vas Korol\n "); break;
-	case 14: 
-		printf("U vas Tuz\n "); break;
-	default: 
-		printf("Neverniy vvod nomera\n ");
+		case 6: 
+			printf("U vas 6-ka\n "); break;
+		case 7: 
+			printf("U vas 7-ka\n "); break;
+		case 8: 
+			printf("U vas 8 - ka\n "); break;
+		case 9: 
+			printf("U vas 9-ka\n "); break;
+		case 10: 
+			printf("U vas 10-ka\n "); break;
+		case 11: 
+			printf("U vas Valet\n "); break;
+		case 12: 
+			printf("U vas Dama\n "); break;
+		case 13: 
+			printf("U vas Korol\n "); break;
+		case 14: 
+			printf("U vas Tuz\n "); break;
+		default: 
+			printf("Neverniy vvod nomera\n ");
 	}
 	return 0;
 }
---
+++ 1sem/pr_04/pr_04_var0/pr_04_var0.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+    SetConsoleOutputCP(1251);
+    double x = 0;
+    double y = 0;
+    printf("Точка %f");
+    return 0;
+}

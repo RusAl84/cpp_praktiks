04cefda4cce77acffea36d3ba33027e2dd6c35b2
Aleksey <rusal@bk.ru>
2023-11-02 09:49:44 +0300

    пр4

--- 1sem/pr_04/pr_04_var0/pr_04_var0.cpp
+++ 1sem/pr_04/pr_04_var0/pr_04_var0.cpp
@@ -11,32 +11,30 @@ int main()
     SetConsoleOutputCP(1251);
     double x = 0;
     double y = 0;
-    
     bool getIntoArea = false;
     //https://www.desmos.com/calculator?lang=ru
-    //проверка для четверти I
-    if ((x >= 0) && (y >= 0)) {
-        if (y <= (1 - x)) {
-            getIntoArea = true; //1
+    if ((x <= 1) && (y <= 1)) {           // Проверка что точка входит в квадрат
+        if ((x >= 0) && (y >= 0)) {       // Проверка для четверти I
+                if (y <= (1 - x))
+                    getIntoArea = true; //1
+                if ((pow(y, 2) + pow(x, 2)) >= 1)
+                    getIntoArea = true; //2
         }
-        if ( (( (pow(y,2) + pow(x, 2)) >= 1) )
-            && ((x <= 1) && (y <= 1)) ) {
-            getIntoArea = true; //2
+        if ((x >= 0) && (y <= 0)) {          // Проверка для четверти II
+                if ((pow(y, 2) + pow(x, 2)) >= 1)
+                    getIntoArea = true; //3
+        }
+        if ((x <= 0) && (y <= 0)) {          // Проверка для четверти III
+                if ((pow(y, 2) + pow(x, 2)) >= 1)
+                    getIntoArea = true; //4
+                if (y <= (1 - x))
+                    getIntoArea = true; //5
+        }
+        if ((x <= 0) && (y >= 0)) {          // Проверка для четверти IV
+                if ((pow(y, 2) + pow(x, 2)) >= 1)
+                    getIntoArea = true; //6
         }
     }
-    //проверка для четверти II
-    if ((x >= 0) && (y <= 0)) {
-
-    }
-    //проверка для четверти III
-    if ((x <= 0) && (y <= 0)) {
-
-    }
-    //проверка для четверти IV
-    if ((x <= 0) && (y >= 0)) {
-
-    }
-
     if (getIntoArea) {
         printf("Точка x = %f  y = %f \n", x, y);
         printf("попадает в заштрихованную область");

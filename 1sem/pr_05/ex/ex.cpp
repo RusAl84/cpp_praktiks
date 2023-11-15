a5f651623728c29ec51506137fd8a58161dc99e9
Aleksey <rusal@bk.ru>
2023-11-16 00:07:17 +0300

    распаковка

--- 1sem/pr_05/ex/ex.cpp
+++ 1sem/pr_05/ex/ex.cpp
@@ -25,21 +25,32 @@ int main()
     unsigned short n; // 0..255
     unsigned short UnitStateWord = 0;
     // Ввод данных
-    c = 7;
+    c = 32;
     f = 1;
-    b = 0;
-    n = 209;
+    b = 1;
+    n = 255;
     // Запаковка
-    c = (UnitStateWord >> c) & 0x1F;
-    f = (UnitStateWord >> f) & 1;
-    b = (UnitStateWord >> b) & 1;
-    //n = UnitStateWord & 0xFF;
+    UnitStateWord = (c & 0x1F) << 11;
+    UnitStateWord |= (f & 1) << 9;
+    UnitStateWord |= (b & 1) << 8;
+    UnitStateWord |= n & 0xFF;
 
-    printf("\nСлово состояния устройства: %04x\n", UnitStateWord);
-    printf("\nСлово состояния устройства в двоичном виде:  %hu\n", UnitStateWord);
+    //c = (UnitStateWord >> c) & 0x1F;
+    //f = (UnitStateWord >> f) & 1;
+    //b = (UnitStateWord >> b) & 1;
+    //n = (UnitStateWord >> n) & 0xFF;
 
+    printf("\nСлово состояния устройства (HEX): %04x\n", UnitStateWord);
+    printf("\nСлово состояния устройства (DEC): %d\n", UnitStateWord);
+    //printf("\nСлово состояния устройства в двоичном виде:  %hu\n", UnitStateWord);
 
-
+    // https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
+    printf("Двоичный код: %c%c%c%c%c%c%c%c:%c%c%c%c%c%c%c%c", BYTE_TO_BINARY(UnitStateWord >> 8), BYTE_TO_BINARY(UnitStateWord));
+    
+    c = (UnitStateWord >> 11) & 0x1F;
+    f = (UnitStateWord >> 9) & 1;
+    b = (UnitStateWord >> 8) & 1;
+    n = UnitStateWord & 0xFF;
 
     return 0;
 }

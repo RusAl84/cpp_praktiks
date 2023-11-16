b01039603d5b2c65c00bad3b21bae198cc5f7509
Aleksey <rusal@bk.ru>
2023-11-16 09:50:06 +0300

    Var 8

--- 1sem/pr_05/pr_05_var8/pr_05_var8.cpp
+++ 1sem/pr_05/pr_05_var8/pr_05_var8.cpp
@@ -18,39 +18,39 @@ int main()
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
     SetConsoleOutputCP(1251);
-    unsigned short N; // 1111 0..15 
-    unsigned short E; // 0..1
-    unsigned short C; // 111111111 0..511
-    unsigned short P; // 0..1
+    // OOOOOCDNNNNNNNNN
+    unsigned short O; // 0b00011111 0..31 
+    unsigned short C; // 0..1
+    unsigned short D; // 0..1
+    unsigned short N; // 0b0000000111111111 0..511
     unsigned short UnitStateWord = 0;
     // Ввод данных
-    N = 15;
-    E = 1;
-    C = 511;
-    P = 1;
-    printf("\nN = %d\n", N);
-    printf("E = %d\n", E);
+    O = 31;
+    C = 1;
+    D = 1;
+    N = 511;
+    printf("\nN = %d\n", O);
     printf("C = %d\n", C);
-    printf("P = %d\n", P);
+    printf("D = %d\n", D);
+    printf("N = %d\n", N);
     // Запаковка
-    // NNNNECCC:CCCCCC0P
-    UnitStateWord = (N & 0x0F) << 12;
-    UnitStateWord |= (E & 1) << 11;
-    UnitStateWord |= (C & 0x1FF) << 2;
-    UnitStateWord |= P & 1;
+    UnitStateWord = (N & 0b00011111) << 11;
+    UnitStateWord |= (C & 1) << 10;
+    UnitStateWord |= (D & 1) << 9;
+    UnitStateWord |= N & 0b0000000111111111;
     printf("\nСлово состояния устройства (16-ричное число от 0 до 0xFFFF): %04x\n", UnitStateWord);
     printf("\nСлово состояния устройства (DEC): %d\n", UnitStateWord);
     // https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
     printf("\nДвоичный код: %c%c%c%c%c%c%c%c:%c%c%c%c%c%c%c%c \n", \
         BYTE_TO_BINARY(UnitStateWord >> 8), BYTE_TO_BINARY(UnitStateWord));
-    // распаковка
-    N = (UnitStateWord >> 12) & 0x0F;
-    E = (UnitStateWord >> 11) & 1;
-    C = (UnitStateWord >> 2) & 0x1FF;
-    P = UnitStateWord & 1;
-    printf("\nN = %d\n", N);
-    printf("E = %d\n", E);
+    // Распаковка
+    O = (UnitStateWord >> 11) & 0b00011111;
+    C = (UnitStateWord >> 10) & 1;
+    D = (UnitStateWord >> 9) & 1;
+    N = UnitStateWord & 0b0000000111111111;
+    printf("\nN = %d\n", O);
     printf("C = %d\n", C);
-    printf("P = %d\n", P);
+    printf("D = %d\n", D);
+    printf("N = %d\n", N);
     return 0;
 }

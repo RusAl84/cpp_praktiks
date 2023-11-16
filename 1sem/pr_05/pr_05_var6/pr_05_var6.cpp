86d81a898e1b624a1227fce6436c242a09e73a25
Aleksey <rusal@bk.ru>
2023-11-16 11:05:44 +0300

    var6

--- 1sem/pr_05/pr_05_var6/pr_05_var6.cpp
+++ 1sem/pr_05/pr_05_var6/pr_05_var6.cpp
@@ -18,33 +18,39 @@ int main()
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
     SetConsoleOutputCP(1251);
-    // HHTTTTTTTTTSSSSS
-    unsigned short H; // 0..3 0b11
-    unsigned short T; // 0..511 0b0000000111111111
-    unsigned short S; // 0..31 0b11111
+    // SSS0D0F0BBBBBBBB
+    unsigned short S; // 0b111 0..3 
+    unsigned short D; // 0..1
+    unsigned short F; // 0..1
+    unsigned short B; // 0b11111111 0..255
     unsigned short UnitStateWord = 0;
     // Ввод данных
-    H = 3;
-    T = 511;
-    S = 31;
-    printf("\nH = %d\n", H);
-    printf("T = %d\n", T);
-    printf("S = %d\n", S);
+    S = 3;
+    D = 1;
+    F = 1;
+    B = 222;
+    printf("\nS = %d\n", S);
+    printf("D = %d\n", D);
+    printf("F = %d\n", F);
+    printf("B = %d\n", B);
     // Запаковка
-    UnitStateWord = (H & 0b11) << 14;
-    UnitStateWord |= (T & 0b0000000111111111) << 5;
-    UnitStateWord |= S & 0b11111;
+    UnitStateWord = (S & 0b111) << 13;
+    UnitStateWord |= (D & 1) << 11;
+    UnitStateWord |= (F & 1) << 9;
+    UnitStateWord |= B & 0b11111111;
     printf("\nСлово состояния устройства (16-ричное число от 0 до 0xFFFF): %04x\n", UnitStateWord);
     printf("\nСлово состояния устройства (DEC): %d\n", UnitStateWord);
     // https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
     printf("\nДвоичный код: %c%c%c%c%c%c%c%c:%c%c%c%c%c%c%c%c \n", \
         BYTE_TO_BINARY(UnitStateWord >> 8), BYTE_TO_BINARY(UnitStateWord));
     // Распаковка
-    H = (UnitStateWord >> 14) & 0b11;
-    T = (UnitStateWord >> 5) & 0b0000000111111111;
-    S = UnitStateWord & 0b0000000111111111;
-    printf("\nH = %d\n", H);
-    printf("T = %d\n", T);
-    printf("S = %d\n", S);
+    S = (UnitStateWord >> 13) & 0b111;
+    D = (UnitStateWord >> 11) & 1;
+    F = (UnitStateWord >> 9) & 1;
+    B = UnitStateWord & 0b11111111;
+    printf("\nS = %d\n", S);
+    printf("D = %d\n", D);
+    printf("F = %d\n", F);
+    printf("B = %d\n", B);
     return 0;
 }

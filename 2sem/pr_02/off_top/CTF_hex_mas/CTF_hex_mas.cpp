7bf206e62fb8df5753090988959e1aa1c0a77017
Администратор <@DESKTOP-9PSOD63>
2024-05-29 09:10:14 +0300

    fix

--- 2sem/pr_02/off_top/CTF_hex_mas/CTF_hex_mas.cpp
+++ 2sem/pr_02/off_top/CTF_hex_mas/CTF_hex_mas.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int mas[] = { 1,2,3,4,5 };
+    int mas[] = { 1000,2000,3000,4000,5000 };
     for (int i=0; i<5;i++)
         std::cout << mas[i] << "\n";
     std::cin >> mas[0];

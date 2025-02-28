1a1fa9c868cb61d7f41a5be0323e0896569e4239
Aleksey <rusal@bk.ru>
2025-02-28 13:37:31 +0300

    fix

--- 2sem2025/PR_03/PR_03.cpp
+++ 2sem2025/PR_03/PR_03.cpp
@@ -1,6 +1,20 @@
 ﻿#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Практическая работа 3\n";
+    auto author = []()
+        {
+            cout << "Русаков Алексей БИСО-02-24" << endl;
+        };
+    author();
+
+    return 0;
 }

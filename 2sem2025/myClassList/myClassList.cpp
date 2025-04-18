cc64ad3f60e8bbcf7d3c151e0f3767e5e77a66b0
Aleksey <rusal@bk.ru>
2025-04-18 14:56:26 +0300

    fix

--- 2sem2025/myClassList/myClassList.cpp
+++ 2sem2025/myClassList/myClassList.cpp
@@ -1,10 +1,15 @@
 ﻿#include <iostream>
+#include <windows.h>
+#include <stdio.h>
 #include "myClassList.h"
 
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     cout << "myClassList\n";
     myClassList* mList = new myClassList();
     mList->addItemFront(1);
@@ -14,4 +19,15 @@ int main()
     mList->addItemBack(2);
     mList->addItemBack(2);
     mList->printMyList();
+    cout << endl << "Count item: " << mList->getCountItem() << endl;
+    mList->editItem(0, 777);
+    mList->printMyList();
+    mList->deleteItem(3);
+    mList->printMyList();
+    mList->insertItem(4, 888);
+    mList->printMyList();
+    cout << endl << "Count item: " << mList->getCountItem() << endl;
+    mList->editItem(5, 555);
+    mList->printMyList();
+
 }
--- 2sem2025/myClassList/myClassList.h
+++ 2sem2025/myClassList/myClassList.h
@@ -52,9 +52,78 @@ public:
             }
             current->next = newItem;
         }
-        newItem->next = myHead;
-
         countItem++;
+    }
+    /// <summary>
+/// Вставка элемента в список
+/// </summary>
+/// <param name="index">индекс после которого вставить</param>
+/// <param name="data">значение которое необходимо вставить</param>
+    void insertItem(int index, int data) {
+        if (not (index >= 0 and index <= countItem and countItem >= 0))
+            return;
+        if (index == 0)
+            addItemFront(data);
+        else {
+            struct node* current = myHead;
+            for (int i = 0; i < index - 1; i++) {
+                current = current->next;
+            }
+            struct node* newItem = new node();
+            newItem->data = data;
+            newItem->next = current->next;
+            current->next = newItem;
+            countItem++;
+        }
+    }
+    /// <summary>
+    /// Изменить элемент списка
+    /// </summary>
+    /// <param name="index">индекс изменяемого элемента</param>
+    /// <param name="data">значение на которое нужно изменить</param>
+    void editItem(int index, int data) {
+        if (index >= 0 and index < countItem and countItem>0) {
+            struct node* current = myHead;
+            for (int i = 0; i < index; i++) {
+                current = current->next;
+                //cout << "+" << current->data;
+            }
+            current->data = data;
+        }
+        else {
+            cout << endl << "Ошибка индекс не в диапазоне";
+        }
+    }
+    /// <summary>
+    /// Функция для удаления элемента из динамического списка
+    /// </summary>
+    /// <param name="index">индекс удаляемого элемента</param>
+    void deleteItem(int index) {
+        if (index >= 0 and index < countItem and countItem>0) {
+            struct node* current = myHead;
+            struct node* old;
+            if (index == 0) {
+                old = myHead;
+                myHead = current->next;
+                delete old;
+                countItem--;
+            }
+            else {
+                int i = 0;
+                while (current) {
+                    if (i == index - 1) {
+                        old = current->next->next;
+                        delete current->next;
+                        current->next = old;
+                        countItem--;
+                        break;
+                    }
+                    i++;
+                    current = current->next;
+                }
+            }
+        }
+
     }
     /// <summary>
     /// Печать списка
@@ -66,6 +135,13 @@ public:
             cout << " " << current->data;
             current = current->next;
         }
+    }
+    /// <summary>
+    /// Получить количество элементов
+    /// </summary>
+    /// <returns>количество элементов</returns>
+    int getCountItem() {
+        return countItem;
     }
 };
 

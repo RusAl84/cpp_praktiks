16273b9e5e0cb4a2c13813d85c3c9262fe00c307
Huawei <Huawei@LAPTOP-FKEMLJA8>
2026-02-20 13:49:51 +0300

    fix

--- 2026_2sem/offTop/Ex1-Person/Ex1-Person.cpp
+++ 2026_2sem/offTop/Ex1-Person/Ex1-Person.cpp
@@ -1,9 +1,14 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include "PersonClass.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!\n";
+    PersonClass sasha;
+    sasha.name = "Санчес";
+    //sasha.age = 17;
+    sasha.setAge(17);
+    sasha.display();
 }
--- 2026_2sem/offTop/Ex1-Person/PersonClass.h
+++ 2026_2sem/offTop/Ex1-Person/PersonClass.h
@@ -1,6 +1,19 @@
 #pragma once
+#include <string>
+using namespace std;
 class PersonClass
 {
-
+public:
+	string name;
+	void setAge(int _age) {
+		age = _age;
+	}
+	void display() {
+		cout << endl << "Имя: " << name;
+		cout << endl << "Возраст: " << age;
+		// endl = "\n"
+	}
+private:
+	int age;
 };
 

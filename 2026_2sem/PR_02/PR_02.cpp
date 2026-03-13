347a214363eec730a7a03c067fc2c869e7c32d71
Huawei <Huawei@LAPTOP-FKEMLJA8>
2026-03-13 12:06:19 +0300

    fix

---
+++ 2026_2sem/PR_02/PR_02.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Практическая работа 2" << endl;
+}

0ead635f90ec8f17d2c1fd2434a7579e2b9be339
Huawei <Huawei@LAPTOP-FKEMLJA8>
2026-03-27 11:26:03 +0300

    start

---
+++ 2026_2sem/PR_03/CapyStringHandle.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class CapyStringHandle
+{
+};
+
---
+++ 2026_2sem/PR_03/PR_03.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Практическая работа 3" << endl;
+    //DinoBaraVirtClass d1(10);
+}

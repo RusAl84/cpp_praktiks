d3c95b4336c08292a91b51b0ee01602cde5644ab
Huawei <Huawei@LAPTOP-FKEMLJA8>
2026-04-10 11:41:23 +0300

    start

--- 2026_2sem/Ex2/Ex2.cpp
+++ 2026_2sem/Ex2/Ex2.cpp
@@ -1,23 +1,24 @@
 ﻿#include <iostream>
 using namespace std;
 void capyFunc(string* data) {
-    (*data) += "PustovoytvTheBest";
-    (*data)[0]='P';
-    (*data).clear();
-    data->clear();
+    (*data) += "   +capyFunc";
+    //(*data)[0]='P';
+    //(*data).clear();
+    //data->clear();
 }
 int main()
 {
-    string  data1 = "Liza i Vika ne s'eli Serezhu";
-    for (int i = 0; i < 100; i++)
+    string  data1 = "zAngelina i gigaSasha i awdNikita i i megaNina i superKira i sigmaLena i alphaNika i ultraVasilina ne s'eli Egorov";
+    for (int i = 0; i < 3; i++)
         data1 += " capy plachet,";
     data1[data1.length() -1] = '!';
     cout << data1.c_str() << endl;
     capyFunc(&data1);
     cout << data1.c_str() << endl;
-    string  *data2  = new string("Capy love Nina!");
-    cout << data2->c_str() << endl;
-    capyFunc(data2);
-    cout << data2->c_str() << endl;
-    cout << (*data2).c_str() << endl;
+//    string  *data2  = new string("Capy love Nina!");
+//    cout << data2->c_str() << endl;
+//    capyFunc(data2);
+//    cout << data2->c_str() << endl;
+//    cout << (*data2).c_str() << endl;
+//
 }
